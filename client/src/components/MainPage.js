c07d2e910ea6de443fc2c7e4a9723923466b1c8d
Yousuf7950 <ysufsheikh@gmail.com>
2022-03-04 07:54:32 -0500

    Updated mainpage
    added component of camera on main to test

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import WebcamCapture from "./Webcam";
 
 export default function MainPage() {
   let navigate = useNavigate();
@@ -9,11 +10,16 @@ export default function MainPage() {
     let path = "/InventoryCategory";
     navigate(path);
   };
+  const routeChange1 = () => {
+    let path1 = "/Webcam";
+    navigate(path1);
+  };
   return (
     <div>
       <div>MainPage</div>
       <h3>Welcome to inventory</h3>
       <button onClick={routeChange}>Create new inventory</button>
+      <button onClick={routeChange1}>Go to camera ( test ) </button>
     </div>
   );
 }
